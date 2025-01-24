c348d25c6e5625d1d706d8219b0f59f506bb59e8
Preston Ong <preston4896@me.com>
2025-01-24 12:23:41 +0800

    minor changes and use docker to build guest program by default

--- zk/risc0/methods/build.rs
+++ zk/risc0/methods/build.rs
@@ -1,3 +1,16 @@
+use risc0_build::{embed_methods_with_options, DockerOptions, GuestOptions};
+use std::collections::HashMap;
+
 fn main() {
-    risc0_build::embed_methods();
+    // Generate Rust source files for the methods crate.
+    embed_methods_with_options(HashMap::from([(
+        "dcap_guest",
+        GuestOptions {
+            features: Vec::new(),
+            use_docker: Some(DockerOptions {
+                root_dir: Some("../".into())
+            }),
+        },
+    )]));
+
 }
